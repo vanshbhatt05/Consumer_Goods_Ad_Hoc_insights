32a13496fe81069d48268c076a613b163ade7b4d
vansh05 <vanshbhatt2002@gmail.com>
2025-08-11 18:31:17 +0530

    Delete req 6.sql

--- req 6.sql
+++
@@ -1,5 +0,0 @@
-SELECT customer_code,customer,avg(pre_invoice_discount_pct) as avg_discount_pert 
- FROM gdb0041.fact_pre_invoice_deductions f join dim_customer c
- using(customer_code) where fiscal_year=2021 and market="India"
- group by customer_code
- order by avg_discount_pert desc limit 5;
